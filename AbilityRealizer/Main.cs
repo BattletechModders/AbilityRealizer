eea7c30c300a18889d003dcfcb113957a63f2ec4
Bill Torode <wmtorode@gmail.com>
2019-11-26 19:20:07 -0500

    get compiling for 1.8

--- AbilityRealizer/Main.cs
+++ AbilityRealizer/Main.cs
@@ -217,7 +217,7 @@ namespace AbilityRealizer
 
             if (pilot.Team != null)
             {
-                reloadAbilities = UpdateAbilitiesFromFaction(pilotDef, pilot.Team.Faction) | reloadAbilities;
+                reloadAbilities = UpdateAbilitiesFromFaction(pilotDef, pilot.Team.FactionValue) | reloadAbilities;
 
                 if (pilot.Team.TeamController == TeamController.Computer)
                     reloadAbilities |= SwapAIAbilities(pilotDef);
@@ -325,24 +325,24 @@ namespace AbilityRealizer
             return reloadAbilities;
         }
 
-        private static bool UpdateAbilitiesFromFaction(PilotDef pilotDef, Faction faction)
+        private static bool UpdateAbilitiesFromFaction(PilotDef pilotDef, FactionValue faction)
         {
             var reloadAbilities = false;
 
-            if (!Settings.FactionAbilities.ContainsKey(faction))
+            if (!Settings.FactionAbilities.ContainsKey(faction.Name))
                 return false;
 
-            foreach (var abilityName in Settings.FactionAbilities[faction])
+            foreach (var abilityName in Settings.FactionAbilities[faction.Name])
             {
                 if (!HasAbilityDef(dataManager, abilityName))
                 {
-                    HBSLog.LogWarning($"Tried to add {abilityName} from faction {faction}, but ability not found!");
+                    HBSLog.LogWarning($"Tried to add {abilityName} from faction {faction.Name}, but ability not found!");
                     continue;
                 }
 
                 if (!pilotDef.abilityDefNames.Contains(abilityName))
                 {
-                    HBSLog.Log($"{pilotDef.Description.Id}: Adding '{abilityName}' from faction '{faction}'");
+                    HBSLog.Log($"{pilotDef.Description.Id}: Adding '{abilityName}' from faction '{faction.Name}'");
                     pilotDef.abilityDefNames.Add(abilityName);
                     reloadAbilities = true;
                 }
